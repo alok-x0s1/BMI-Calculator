f9625d432081a0e9ce386f3c9bbdc4f9c20932fb
Alok Yadav <yalok6321@gmail.com>
2024-04-12 15:12:55 +0530

    Change in Under weight conditions

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.querySelector('form').addEventListener('submit', (e) => {
     if (height === "" || height < 0 || isNaN(height)) {
         result.innerHTML = "Please enter a valide height"
     } else if (weight === "" || weight < 0 || isNaN(weight)) {
-        result.innerHTML = "Please enter a valide weight"
+        result.innerHTML = "Please enter a valid weight"
     } else {
         const bmi = (weight / ((height*height) / 10000)).toFixed(2)
-        if (bmi < 18.6) {
-            result.innerHTML = `Your BMI is : ${bmi} and you are Normal Weight`
-        } else if (bmi > 18.6 && bmi < 29.9) {
+        if (bmi < 18.5) {
+            result.innerHTML = `Your BMI is : ${bmi} and you are Under Weight`
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
             result.innerHTML = `Your BMI is : ${bmi} and you are Normal Weight`
         } else {
             result.innerHTML = `Your BMI is : ${bmi} and you are Over Weight`
